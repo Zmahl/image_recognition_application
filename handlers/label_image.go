bba302e02f09af8f051c17fe245f84bdf87864f7
Zmahl <zach.mahl@udel.edu>
2023-11-18 15:08:49 -0500

    adding helper functions to break up functionality

--- handlers/label_image.go
+++ handlers/label_image.go
@@ -1,1 +1,83 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+	"google.golang.org/appengine"
+)
+
+var (
+	CLOUD_BUCKET_CREDENTIALS = os.Getenv("cloud_bucket_service_account_credentials")
+	BUCKET_NAME              = os.Getenv("bucket_name")
+	VISION_API_KEY           = os.Getenv("vision_api_key")
+)
+
+var storageClient *storage.Client
+
+func HandleFileUpload(c *gin.Context) {
+	bucket := BUCKET_NAME
+
+	var err error
+
+	ctx := appengine.NewContext(c.Request)
+
+	storageClient, err = storage.NewClient(ctx, option.WithCredentialsFile("keys.json"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return
+	}
+
+	f, uploadedFile, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return
+	}
+
+	defer f.Close()
+
+	sw := storageClient.Bucket(bucket).Object(uploadedFile.Filename).NewWriter(ctx)
+
+	if _, err := io.Copy(sw, f); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return
+	}
+
+	if err := sw.Close(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return
+	}
+
+	u, err := url.Parse("/" + bucket + "/" + sw.Attrs().Name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"Error":   true,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":  "file uploaded successfully",
+		"pathname": u.EscapedPath(),
+	})
+
+}
