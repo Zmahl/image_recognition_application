40164cbe7e5591ba0c27967c038f4deaf30896aa
Zmahl <zach.mahl@udel.edu>
2023-11-18 14:57:35 -0500

    change to gin framework

--- main.go
+++ main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	receiveImageHandler := http.HandlerFunc(receiveImage)
-	http.Handle("/identify-image", receiveImageHandler)
-	http.ListenAndServe(":8080", nil)
-}
+	r := gin.Default()
 
-func receiveImage(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(32 << 20)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	file, h, err := r.FormFile("photo")
-
-	tmpfile, err := os.Create("./images" + h.Filename)
-	defer tmpfile.Close()
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	_, err = io.Copy(tmpfile, file)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(200)
-	return
+	r.Run()
 }
