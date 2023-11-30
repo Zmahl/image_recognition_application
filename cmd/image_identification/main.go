d47d1403df70a7412f34bd2866207683f9ff7252
Zmahl <zach.mahl@udel.edu>
2023-11-29 23:19:01 -0500

    added config package and getEnv helper function

--- cmd/image_identification/main.go
+++ cmd/image_identification/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"github.com/Zmahl/image_recognition_application/pkg/auth"
+	"log"
+	"os"
+
+	"github.com/Zmahl/image_recognition_application/pkg/config"
 	"github.com/Zmahl/image_recognition_application/pkg/label"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-var CloudCredentials = auth.NewCloudCredentials()
+var conf *config.Config
+
+func init() {
+
+	// If there is no .env file to load, the program will exit
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+		os.Exit(1)
+	}
+}
 
 func main() {
+
 	r := gin.Default()
-	r.POST("labels/image", label.LabelImageHandler(CloudCredentials))
+	r.POST("labels/image", label.LabelImageHandler(conf))
 	r.Run()
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import "os"
+
+type GoogleCloudStorageConfig struct {
+	BucketName           string
+	GoogleServiceAccount string
+}
+
+type VisionConfig struct {
+	VisionAPIKey string
+}
+
+type Config struct {
+	Storage GoogleCloudStorageConfig
+	Vision  VisionConfig
+}
+
+func New() *Config {
+	return &Config{
+		Storage: GoogleCloudStorageConfig{
+			BucketName:           getEnv("BUCKET_NAME", ""),
+			GoogleServiceAccount: getEnv("GOOGLE_CLOUD_SERVICE_ACCOUNT", ""),
+		},
+		Vision: VisionConfig{
+			VisionAPIKey: getEnv("VISION_API_KEY", ""),
+		},
+	}
+}
+
+// Sets values for keys from env
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
