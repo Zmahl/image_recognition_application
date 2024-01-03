e95d6a7feffbcf0d66101c67ceedd4f364187853
Zmahl <zach.mahl@udel.edu>
2024-01-02 23:10:25 -0500

    updated tests

--- pkg/config/config_gcp_test.go
+++ pkg/config/config_gcp_test.go
@@ -1,15 +1,23 @@
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Zmahl/image_recognition_application/pkg/label"
+	"github.com/Zmahl/image_recognition_application/pkg/storage"
+)
 
 const gcpEnv = "GCP"
 
-func CreateTestConfig(cloudEnv string) ApplicationConfig {
-	return ApplicationConfig{}
+type TestApplicationConfig struct {
+	Storage  storage.StorageProvider
+	Labeller label.Labeller
 }
 
+// All tests are initialized without using the GCP environment setup
+
 func TestEmptyConfig(t *testing.T) {
-	t.Run("empty config has empty fields and returns error", func(t *testing.T) {
+	t.Run("empty config due to missing test environment", func(t *testing.T) {
 		cloudEnv := ""
 
 		config := CreateAppConfig(cloudEnv)
@@ -26,18 +34,23 @@ func TestEmptyConfig(t *testing.T) {
 }
 
 func TestIncompleteConfig(t *testing.T) {
-	t.Run("empty config has empty fields and returns error", func(t *testing.T) {
-		cloudEnv := gcpEnv
+	cloudEnv := gcpEnv
+	config := CreateAppConfig(cloudEnv)
 
-		config := CreateAppConfig(cloudEnv)
-
-		if config.Storage != nil {
-			t.Errorf("storage should not exist")
+	t.Run("config missing storage fields", func(t *testing.T) {
+		if config.Storage.GetBucket() != "" {
+			t.Errorf("storage bucket name should not exist")
 		}
 
-		if config.Labeller != nil {
-			t.Errorf("labeller should not exist")
+		if config.Storage.GetServiceAccount() != "" {
+			t.Errorf("storage bucket name should not exist")
 		}
 
+	})
+
+	t.Run("config missing labeller fields", func(t *testing.T) {
+		if config.Labeller.GetLabelCredentials() != "" {
+			t.Errorf("google vision api key should not exist")
+		}
 	})
 }
