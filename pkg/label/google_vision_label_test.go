2e73760457a85f37792439cb3fb225ec22be3ada
Zmahl <zach.mahl@udel.edu>
2024-01-04 20:20:33 -0500

    added mock context function

--- pkg/label/google_vision_label_test.go
+++ pkg/label/google_vision_label_test.go
@@ -1,6 +1,8 @@
 package label
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +17,16 @@ func (d DummyGoogleVision) LabelImage(c *gin.Context, url string) {
 
 func CreateDummyGoogleVision() DummyGoogleVision {
 	return DummyGoogleVision{}
+}
+
+func CreateMockContext() *gin.Engine {
+	router := gin.Default()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "labels/image", nil)
+	router.ServeHTTP(w, req)
+
+	return router
 }
 
 func TestVision(t *testing.T) {
